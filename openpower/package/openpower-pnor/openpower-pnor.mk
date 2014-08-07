31829a4d08bc52db2c8ce225ff1cc77f232261dc
Brian Silver <bsilver@us.ibm.com>
2014-08-07 15:50:15 -0500

    Generalize targeting to system xml/targeting

--- openpower/package/openpower-pnor/openpower-pnor.mk
+++ openpower/package/openpower-pnor/openpower-pnor.mk
@@ -4,30 +4,29 @@
 #
 ################################################################################
 
+# remove the quotes from the XML/Targeting package as
+# make doesn't care for quotes in the dependencies.
+XML_PACKAGE=$(subst $\",,$(BR2_OPENPOWER_XML_PACKAGE))
+
 OPENPOWER_PNOR_VERSION = 0855f1b9683e13583248a64454fe13fb02fd1b87
 OPENPOWER_PNOR_SITE = $(call github,open-power,pnor,$(OPENPOWER_PNOR_VERSION))
 OPENPOWER_PNOR_LICENSE = Apache-2.0
-OPENPOWER_PNOR_DEPENDENCIES = hostboot hostboot-binaries openpower-targeting skiboot host-openpower-ffs
+OPENPOWER_PNOR_DEPENDENCIES = hostboot hostboot-binaries $(XML_PACKAGE) skiboot host-openpower-ffs
 
 OPENPOWER_PNOR_INSTALL_IMAGES = YES
 OPENPOWER_PNOR_INSTALL_TARGET = NO
 
-#OPENPOWER_PNOR_FILENAME = palmetto.pnor
-#OPENPOWER_PNOR_TARGETING_FILE = $(STAGING_DIR)/openpower_targeting/PALMETTO_HB_targeting.bin
-
-
-#ECC_TOOL_DIR ???
-OPENPOWER_TARGETING_DIR=$(STAGING_DIR)/openpower_targeting/
+OPENPOWER_TARGETING_DIR=$(STAGING_DIR)/$(XML_PACKAGE)/
 HOSTBOOT_IMAGE_DIR=$(STAGING_DIR)/hostboot_build_images/
 HOSTBOOT_BINARY_DIR = $(STAGING_DIR)/hostboot_binaries/
 OPENPOWER_PNOR_SCRATCH_DIR = $(STAGING_DIR)/openpower_pnor_scratch/
 
 define OPENPOWER_PNOR_INSTALL_IMAGES_CMDS
         mkdir -p $(OPENPOWER_PNOR_SCRATCH_DIR)
-        $(TARGET_MAKE_ENV) $(@D)/update_image_$(BR2_OPENPOWER_CONFIG_NAME).pl -op_target_dir $(STAGING_DIR)/openpower_targeting/ -hb_image_dir $(HOSTBOOT_IMAGE_DIR) -scratch_dir $(OPENPOWER_PNOR_SCRATCH_DIR) -hb_binary_dir $(HOSTBOOT_BINARY_DIR) -targeting_binary_filename $(BR2_OPENPOWER_TARGETING_ECC_FILENAME) -targeting_binary_source $(BR2_OPENPOWER_TARGETING_BIN_FILENAME)
+        $(TARGET_MAKE_ENV) $(@D)/update_image_$(BR2_OPENPOWER_CONFIG_NAME).pl -op_target_dir $(OPENPOWER_TARGETING_DIR) -hb_image_dir $(HOSTBOOT_IMAGE_DIR) -scratch_dir $(OPENPOWER_PNOR_SCRATCH_DIR) -hb_binary_dir $(HOSTBOOT_BINARY_DIR) -targeting_binary_filename $(BR2_PALMETTO_TARGETING_ECC_FILENAME) -targeting_binary_source $(BR2_PALMETTO_TARGETING_BIN_FILENAME)
 
         mkdir -p $(STAGING_DIR)/pnor/
-        $(TARGET_MAKE_ENV) $(@D)/create_pnor_image.pl -xml_layout_file $(@D)/$(BR2_OPENPOWER_PNOR_XML_LAYOUT_FILENAME) -pnor_filename $(STAGING_DIR)/pnor/$(BR2_OPENPOWER_PNOR_FILENAME) -hb_image_dir $(HOSTBOOT_IMAGE_DIR) -scratch_dir $(OPENPOWER_PNOR_SCRATCH_DIR) -outdir $(STAGING_DIR)/pnor/ -payload $(BINARIES_DIR)/$(BR2_SKIBOOT_LID_NAME) -sbe_binary_filename $(BR2_HOSTBOOT_BINARY_SBE_FILENAME) -sbec_binary_filename $(BR2_HOSTBOOT_BINARY_SBEC_FILENAME) -targeting_binary_filename $(BR2_OPENPOWER_TARGETING_ECC_FILENAME)
+        $(TARGET_MAKE_ENV) $(@D)/create_pnor_image.pl -xml_layout_file $(@D)/$(BR2_OPENPOWER_PNOR_XML_LAYOUT_FILENAME) -pnor_filename $(STAGING_DIR)/pnor/$(BR2_OPENPOWER_PNOR_FILENAME) -hb_image_dir $(HOSTBOOT_IMAGE_DIR) -scratch_dir $(OPENPOWER_PNOR_SCRATCH_DIR) -outdir $(STAGING_DIR)/pnor/ -payload $(BINARIES_DIR)/$(BR2_SKIBOOT_LID_NAME) -sbe_binary_filename $(BR2_HOSTBOOT_BINARY_SBE_FILENAME) -sbec_binary_filename $(BR2_HOSTBOOT_BINARY_SBEC_FILENAME) -targeting_binary_filename $(BR2_PALMETTO_TARGETING_ECC_FILENAME)
 
 	$(INSTALL) $(STAGING_DIR)/pnor/$(BR2_OPENPOWER_PNOR_FILENAME) $(BINARIES_DIR)
 endef
---
+++ openpower/package/palmetto-xml/palmetto-xml.mk
@@ -0,0 +1,25 @@
+################################################################################
+#
+# palmetto_xml
+#
+################################################################################
+
+PALMETTO_XML_VERSION = adda3365283b7bcfcd1f19373b01acfc7669a84f
+PALMETTO_XML_SITE = $(call github,open-power,hostboot-targeting,$(PALMETTO_XML_VERSION))
+PALMETTO_XML_LICENSE = Apache-2.0
+PALMETTO_XML_DEPENDENCIES = hostboot-install-images
+
+PALMETTO_XML_INSTALL_IMAGES = YES
+PALMETTO_XML_INSTALL_TARGET = NO
+
+define PALMETTO_XML_INSTALL_IMAGES_CMDS
+        mkdir -p $(STAGING_DIR)/$(BR2_OPENPOWER_XML_PACKAGE);
+
+        $(STAGING_DIR)/hostboot_build_images/mergexml.sh $(@D)/$(BR2_PALMETTO_SYSTEM_XML_FILENAME) $(STAGING_DIR)/hostboot_build_images/attribute_types.xml $(STAGING_DIR)/hostboot_build_images/attribute_types_hb.xml $(STAGING_DIR)/hostboot_build_images/target_types_merged.xml $(STAGING_DIR)/hostboot_build_images/target_types_hb.xml $(@D)/$(BR2_PALMETTO_MRW_XML_FILENAME) > $(STAGING_DIR)/$(BR2_OPENPOWER_XML_PACKAGE)/temporary_hb.hb.xml;
+
+        $(STAGING_DIR)/hostboot_build_images/xmltohb.pl  --hb-xml-file=$(STAGING_DIR)/$(BR2_OPENPOWER_XML_PACKAGE)/temporary_hb.hb.xml --fapi-attributes-xml-file=$(STAGING_DIR)/hostboot_build_images/fapiattrs.xml --src-output-dir=none --img-output-dir=$(STAGING_DIR)/$(BR2_OPENPOWER_XML_PACKAGE)/ --vmm-consts-file=$(STAGING_DIR)/hostboot_build_images/vmmconst.h --noshort-enums
+
+        mv $(STAGING_DIR)/$(BR2_OPENPOWER_XML_PACKAGE)/targeting.bin $(STAGING_DIR)/$(BR2_OPENPOWER_XML_PACKAGE)/$(BR2_PALMETTO_TARGETING_BIN_FILENAME)
+endef
+
+$(eval $(generic-package))
