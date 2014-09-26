13c43445f991a2829b70c83dbef76e30065b531d
Dean Sanner <dsanner@us.ibm.com>
2014-09-26 11:57:20 -0500

    Update hostboot to include console output

--- openpower/package/hostboot-binaries/hostboot_binaries.mk
+++ openpower/package/hostboot-binaries/hostboot_binaries.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-HOSTBOOT_BINARIES_VERSION = f06d3d171ed69f0553a3aa02685d232a1dec52dc
-HOSTBOOT_BINARIES_SITE = $(call github,sannerd,hostboot-binaries,$(HOSTBOOT_BINARIES_VERSION))
+HOSTBOOT_BINARIES_VERSION = da5ca565422d157e7225d3f20c7ff06f63610f49
+HOSTBOOT_BINARIES_SITE = $(call github,open-power,hostboot-binaries,$(HOSTBOOT_BINARIES_VERSION))
 HOSTBOOT_BINARIES_LICENSE = Apache-2.0
 
 HOSTBOOT_BINARIES_INSTALL_IMAGES = YES
